3f1b2bc260b7708aebd0dfb8df3964a4083cdd2f
张奥 <2515912396@qq.com>
2019-10-15 14:46:40 +0800

    提交

---
+++ DJFly/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DJFly
+//
+//  Created by 张奥 on 2019/10/15.
+//  Copyright © 2019 张奥. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DJFly/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DJFly
+//
+//  Created by 张奥 on 2019/10/15.
+//  Copyright © 2019 张奥. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
